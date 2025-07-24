1997e6493de528080b81746c68d5ee8a6300b40a
Frontkick <jainsoumya7378@gmail.com>
2025-07-24 12:32:24 +0530

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# Sample DataFrame
+data = {
+    'name': ['Alice', 'Bob', 'Charlie'],
+    'age': [25, 30, 35]
+}
+df = pd.DataFrame(data, index=['a', 'b', 'c'])
+
+
+df.loc['b']     # Gets the row with label 'b'
+print(df.iloc[1])     
